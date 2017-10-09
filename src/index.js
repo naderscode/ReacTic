e49fa0c13f9dc9d788119f8463a0b598393d5520
naderscode <naderscode@gmail.com>
2017-10-08 22:00:43 -0400

    update handleClick function to check for winner and return

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -47,7 +50,7 @@ class Board extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-    
+
     return (
       <div>
         <div className="status">{status}</div>
